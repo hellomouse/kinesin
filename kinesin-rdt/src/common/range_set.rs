3b44c72ce72be561e4e9b9f403607352313b347a
iczero <iczero4@gmail.com>
2023-03-13 09:24:49 -0400

    RangeSet refactor
    - allow more range types
    - check and prevent some instances of wrong behavior

--- kinesin-rdt/src/common/range_set.rs
+++ kinesin-rdt/src/common/range_set.rs
@@ -1,8 +1,10 @@
 use std::collections::BTreeMap;
-use std::ops::Range;
+use std::ops::{Bound, Range, RangeBounds};
 
+// TODO: this is apparently massively horrible
 /// Set of ranges implemented with a BTreeMap. No overlapping ranges are
-/// allowed. Consecutive ranges are merged.
+/// allowed. Consecutive ranges are merged. Representable ranges are
+/// [0, u64::MAX).
 pub struct RangeSet {
     /// Backing map, where key = start and value = length.
     map: BTreeMap<u64, u64>,
@@ -15,6 +17,10 @@ impl RangeSet {
             map: BTreeMap::new(),
             max_size,
         }
+    }
+
+    pub fn unlimited() -> RangeSet {
+        Self::new(usize::MAX)
     }
 
     /// Test if a single value is contained in the set.
@@ -43,7 +49,8 @@ impl RangeSet {
     }
 
     fn _direct_insert(&mut self, new_range: Range<u64>) {
-        self.map.insert(new_range.start, new_range.end - new_range.start);
+        self.map
+            .insert(new_range.start, new_range.end - new_range.start);
     }
 
     fn _max_checked_insert(&mut self, new_range: Range<u64>) -> bool {
@@ -60,30 +67,29 @@ impl RangeSet {
         let range_iter = self.map.range(..=new_range.end);
         let mut to_remove: Vec<u64> = Vec::new();
         for (&start, &len) in range_iter.rev() {
+            let end = start + len;
             if start > new_range.start {
-                if start + len > new_range.end {
+                if end > new_range.end {
                     // intersecting or immediately following range extends
                     // past end of new range
-                    new_range.end = start + len;
+                    new_range.end = end;
                 } else {
                     // intersecting range entirely contained with in new range
                 }
                 to_remove.push(start);
+            } else if end < new_range.start {
+                // new range is entirely after current range (no intersection)
+                // no more ranges to search
+                break;
+            } else if end < new_range.end {
+                // intersecting range or immediately preceding range extends
+                // past start of new range
+                new_range.start = start;
+                to_remove.push(start);
             } else {
-                if start + len < new_range.start {
-                    // new range is entirely after current range (no intersection)
-                    // no more ranges to search
-                    break;
-                } else if start + len < new_range.end {
-                    // intersecting range or immediately preceding range extends
-                    // past start of new range
-                    new_range.start = start;
-                    to_remove.push(start);
-                } else {
-                    // new range is entirely contained within existing range
-                    // Initial should've handled this
-                    unreachable!();
-                }
+                // new range is entirely contained within existing range
+                // Initial should've handled this
+                unreachable!();
             }
         }
         for s in to_remove {
@@ -95,12 +101,16 @@ impl RangeSet {
 
     /// Insert a range into the set
     pub fn insert_range(&mut self, new_range: Range<u64>) -> bool {
+        if new_range.start == new_range.end {
+            panic!("cannot insert zero-length range");
+        }
         let mut range_iter = self.map.range(..=new_range.end);
         if let Some((&start, &len)) = range_iter.next_back() {
-            if start <= new_range.start && start + len >= new_range.end {
+            let end = start + len;
+            if start <= new_range.start && end >= new_range.end {
                 // range already covered in set
                 true
-            } else if start + len < new_range.start {
+            } else if end < new_range.start {
                 // new range is after all existing ranges
                 self._max_checked_insert(new_range)
             } else {
@@ -116,30 +126,47 @@ impl RangeSet {
     }
 
     /// Remove range from set
-    pub fn remove_range(&mut self, to_remove: Range<u64>) -> usize {
+    pub fn remove_range<R: RangeBounds<u64>>(&mut self, to_remove: R) -> usize {
+        let lower_bound = match to_remove.start_bound() {
+            Bound::Included(start) => *start,
+            Bound::Excluded(start) => start.checked_add(1).expect("range out of bounds"),
+            Bound::Unbounded => 0,
+        };
+        let upper_bound = match to_remove.end_bound() {
+            Bound::Included(end) => end.checked_add(1).expect("range out of bounds"),
+            Bound::Excluded(end) => *end,
+            Bound::Unbounded => u64::MAX,
+        };
+
+        if lower_bound == upper_bound {
+            panic!("cannot remove zero-length range");
+        }
+
         let mut affected = 0;
-        let range_iter = self.map.range(..to_remove.end);
+        let range_iter = self.map.range((Bound::Unbounded, to_remove.end_bound()));
         let mut pending_ops: Vec<(u64, Option<u64>)> = Vec::new();
+
         for (&start, &len) in range_iter.rev() {
-            if start + len <= to_remove.start {
+            let end = start + len;
+            if end <= lower_bound {
                 // no more ranges could possibly match
                 break;
-            } else if start + len <= to_remove.end {
-                if start >= to_remove.start {
+            } else if end <= upper_bound {
+                if start >= lower_bound {
                     // range is entirely contained within to_remove
                     pending_ops.push((start, None));
                     affected += 1;
                 } else {
                     // range extends into to_remove
-                    pending_ops.push((start, Some(to_remove.start - start)));
+                    pending_ops.push((start, Some(lower_bound - start)));
                     affected += 1;
                     break;
                 }
-            } else if start + len > to_remove.end {
-                if start < to_remove.start {
+            } else if end > upper_bound {
+                if start < lower_bound {
                     // current range includes to_remove, split range
-                    pending_ops.push((start, Some(to_remove.start - start)));
-                    pending_ops.push((to_remove.end, Some((start + len) - to_remove.end)));
+                    pending_ops.push((start, Some(lower_bound - start)));
+                    pending_ops.push((upper_bound, Some(end - upper_bound)));
                     affected += 1;
                     break;
                 } else {
@@ -148,7 +175,7 @@ impl RangeSet {
                     // delete old range
                     pending_ops.push((start, None));
                     // insert trimmed range
-                    pending_ops.push((to_remove.end, Some((start + len) - to_remove.end)));
+                    pending_ops.push((upper_bound, Some(end - upper_bound)));
                     affected += 1;
                 }
             } else {
@@ -167,33 +194,16 @@ impl RangeSet {
 
     /// Peek first value in set
     pub fn peek_first(&self) -> Option<Range<u64>> {
-        self.map.first_key_value().map(|(&start, &len)| { start..(start + len) })
+        self.map
+            .first_key_value()
+            .map(|(&start, &len)| start..(start + len))
     }
 
     /// Peek last value in set
     pub fn peek_last(&self) -> Option<Range<u64>> {
-        self.map.last_key_value().map(|(&start, &len)| { start..(start + len) })
-    }
-
-    /// Remove all ranges below a given value. Truncate ranges if they include the value.
-    pub fn remove_until_from_first(&mut self, val: u64) {
-        let mut to_remove: Vec<u64> = Vec::new();
-        let mut adjust_start: Option<u64> = None;
-        for (&start, &len) in self.map.range(..=val) {
-            if start + len <= val {
-                to_remove.push(start);
-            } else {
-                adjust_start = Some(start);
-            }
-        }
-        for s in to_remove {
-            self.map.remove(&s);
-        }
-        if let Some(adjust_start) = adjust_start {
-            let len = self.map.remove(&adjust_start).unwrap();
-            let end = adjust_start + len;
-            self.map.insert(val, end - val);
-        }
+        self.map
+            .last_key_value()
+            .map(|(&start, &len)| start..(start + len))
     }
 
     /// Dump all ranges in set
@@ -207,7 +217,7 @@ impl RangeSet {
 #[cfg(test)]
 mod test {
     use super::RangeSet;
-    
+
     fn ensure_consistency(rs: &RangeSet) {
         assert!(rs.map.len() > 0);
         let mut iter = rs.map.iter();
@@ -216,19 +226,22 @@ mod test {
 
         for (&start, &len) in iter {
             assert!(start > last_end);
-            last_end = start + len;
+            assert!(len > 0);
+            let did_overflow;
+            (last_end, did_overflow) = start.overflowing_add(len);
+            assert!(!did_overflow);
         }
     }
 
     #[test]
     fn insert_distinct_range() {
-        let mut rs = RangeSet::new(usize::MAX);
+        let mut rs = RangeSet::unlimited();
         assert!(rs.insert_range(0..10));
         assert!(rs.insert_range(20..30));
         assert!(rs.insert_range(40..50));
         assert!(rs.insert_range(60..70));
         assert!(rs.insert_range(80..90));
-        
+
         assert!(rs.has_value(0));
         assert!(rs.has_value(1));
         assert!(!rs.has_value(10));
@@ -246,7 +259,7 @@ mod test {
 
     #[test]
     fn insert_overlapping_range() {
-        let mut rs = RangeSet::new(usize::MAX);
+        let mut rs = RangeSet::unlimited();
         // overlapping ranges
         assert!(rs.insert_range(0..10));
         assert!(rs.insert_range(5..15));
@@ -264,6 +277,8 @@ mod test {
         assert!(!rs.has_value(75));
         assert!(rs.insert_range(70..80));
         assert_eq!(rs.peek_last(), Some(50..100));
+        assert!(rs.insert_range(100..101));
+        assert_eq!(rs.peek_last(), Some(50..101));
 
         assert!(rs.has_value(0));
         assert!(rs.has_value(8));
@@ -283,17 +298,17 @@ mod test {
 
     #[test]
     fn remove_until() {
-        let mut rs = RangeSet::new(usize::MAX);
+        let mut rs = RangeSet::unlimited();
         assert!(rs.insert_range(0..10));
         assert!(rs.insert_range(20..30));
         assert!(rs.insert_range(40..50));
         assert!(rs.insert_range(60..70));
         assert!(rs.insert_range(80..90));
 
-        rs.remove_until_from_first(15);
+        rs.remove_range(..15);
         assert_eq!(rs.peek_first(), Some(20..30));
 
-        rs.remove_until_from_first(25);
+        rs.remove_range(..25);
         assert_eq!(rs.peek_first(), Some(25..30));
 
         ensure_consistency(&rs);
@@ -325,17 +340,17 @@ mod test {
 
     #[test]
     fn remove_range() {
-        let mut rs = RangeSet::new(usize::MAX);
+        let mut rs = RangeSet::unlimited();
         assert!(rs.insert_range(0..10));
         assert!(rs.insert_range(20..30));
         assert!(rs.insert_range(40..50));
-        
+
         assert_eq!(rs.remove_range(5..45), 3);
         assert_eq!(rs.map.len(), 2);
         assert_eq!(rs.peek_first(), Some(0..5));
         assert_eq!(rs.peek_last(), Some(45..50));
 
-        rs.remove_until_from_first(100);
+        rs.remove_range(..100);
         assert_eq!(rs.map.len(), 0);
 
         assert!(rs.insert_range(0..100));
