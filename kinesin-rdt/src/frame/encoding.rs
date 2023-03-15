26c39f70260cfdcc359449cc5dcbaecc2615be65
iczero <iczero4@gmail.com>
2023-03-15 14:25:57 -0400

    decoding should be fallible

--- kinesin-rdt/src/frame/encoding.rs
+++ kinesin-rdt/src/frame/encoding.rs
@@ -1,3 +1,5 @@
+//! Frame encoding utilities
+
 /// determine how many bytes are required to encode a varint8
 pub fn varint8_size(n: u64) -> Option<usize> {
     if n < 2u64.pow(8 - 2) {
@@ -40,25 +42,38 @@ pub fn write_varint8(buf: &mut [u8], n: u64) -> Option<usize> {
 }
 
 /// read varint8 from buffer, returning (value, size)
-pub fn read_varint8(buf: &mut [u8]) -> (u64, usize) {
+pub fn read_varint8(buf: &[u8]) -> Result<(u64, usize), ()> {
+    if buf.len() < 1 {
+        return Err(());
+    }
     let length = buf[0] >> 6;
     match length {
-        0 => {
-            ((buf[0] & (u8::MAX >> 2)) as u64, 1)
-        },
+        0 => Ok(((buf[0] & (u8::MAX >> 2)) as u64, 1)),
         1 => {
-            let val = u16::from_be_bytes(buf[0..2].try_into().unwrap());
-            ((val & (u16::MAX >> 2)) as u64, 2)
-        },
+            if buf.len() >= 2 {
+                let val = u16::from_be_bytes(buf[0..2].try_into().unwrap());
+                Ok(((val & (u16::MAX >> 2)) as u64, 2))
+            } else {
+                Err(())
+            }
+        }
         2 => {
-            let val = u32::from_be_bytes(buf[0..4].try_into().unwrap());
-            ((val & (u32::MAX >> 2)) as u64, 4)
-        },
+            if buf.len() >= 4 {
+                let val = u32::from_be_bytes(buf[0..4].try_into().unwrap());
+                Ok(((val & (u32::MAX >> 2)) as u64, 4))
+            } else {
+                Err(())
+            }
+        }
         3 => {
-            let val = u64::from_be_bytes(buf[0..8].try_into().unwrap());
-            (val & (u64::MAX >> 2), 8)
-        },
-        _ => unreachable!()
+            if buf.len() >= 8 {
+                let val = u64::from_be_bytes(buf[0..8].try_into().unwrap());
+                Ok((val & (u64::MAX >> 2), 8))
+            } else {
+                Err(())
+            }
+        }
+        _ => unreachable!(),
     }
 }
 
@@ -97,24 +112,32 @@ pub fn write_varint4(buf: &mut [u8], n: u32) -> Option<usize> {
 }
 
 /// read varint4 from buffer, returning (value, size)
-pub fn read_varint4(buf: &mut [u8]) -> (u32, usize) {
+pub fn read_varint4(buf: &[u8]) -> Result<(u32, usize), ()> {
+    if buf.len() < 1 {
+        return Err(());
+    }
     let length = buf[0] >> 6;
     match length {
-        0b00 => {
-            ((buf[0] & (u8::MAX >> 2)) as u32, 1)
-        },
+        0b00 => Ok(((buf[0] & (u8::MAX >> 2)) as u32, 1)),
         0b01 => {
-            let val = u16::from_be_bytes(buf[0..2].try_into().unwrap());
-            ((val & (u16::MAX >> 2)) as u32, 2)
-        },
+            if buf.len() >= 2 {
+                let val = u16::from_be_bytes(buf[0..2].try_into().unwrap());
+                Ok(((val & (u16::MAX >> 2)) as u32, 2))
+            } else {
+                Err(())
+            }
+        }
         0b10 | 0b11 => {
-            let val = u32::from_be_bytes(buf[0..4].try_into().unwrap());
-            (val & (u32::MAX >> 1), 4)
-        },
-        _ => unreachable!()
+            if buf.len() >= 4 {
+                let val = u32::from_be_bytes(buf[0..4].try_into().unwrap());
+                Ok((val & (u32::MAX >> 1), 4))
+            } else {
+                Err(())
+            }
+        }
+        _ => unreachable!(),
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -126,29 +149,31 @@ mod test {
         assert_eq!(varint8_size(0), Some(1));
         assert_eq!(write_varint8(&mut buf, 0), Some(1));
         assert_eq!(buf, [0, 5, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint8(&mut buf), (0, 1));
+        assert_eq!(read_varint8(&buf), Ok((0, 1)));
 
         assert_eq!(varint8_size(16), Some(1));
         assert_eq!(write_varint8(&mut buf, 16), Some(1));
         assert_eq!(buf, [16, 5, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint8(&mut buf), (16, 1));
+        assert_eq!(read_varint8(&buf), Ok((16, 1)));
 
         assert_eq!(varint8_size(128), Some(2));
         assert_eq!(write_varint8(&mut buf, 128), Some(2));
         assert_eq!(buf, [64, 128, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint8(&mut buf), (128, 2));
+        assert_eq!(read_varint8(&buf), Ok((128, 2)));
 
         assert_eq!(varint8_size(57_829_138), Some(4));
         assert_eq!(write_varint8(&mut buf, 57_829_138), Some(4));
         assert_eq!(buf, [0x83, 0x72, 0x67, 0x12, 5, 5, 5, 5]);
-        assert_eq!(read_varint8(&mut buf), (57_829_138, 4));
+        assert_eq!(read_varint8(&buf), Ok((57_829_138, 4)));
 
         assert_eq!(varint8_size(3_933_194_752_826_327_366), Some(8));
         assert_eq!(write_varint8(&mut buf, 3_933_194_752_826_327_366), Some(8));
         assert_eq!(buf, [0xf6, 0x95, 0x83, 0xc9, 0xea, 0xa4, 0xc1, 0x46]);
-        assert_eq!(read_varint8(&mut buf), (3_933_194_752_826_327_366, 8));
+        assert_eq!(read_varint8(&buf), Ok((3_933_194_752_826_327_366, 8)));
 
         assert_eq!(varint8_size(9_000_000_000_000_000_000), None);
+
+        assert_eq!(read_varint8(&[0xf6]), Err(()))
     }
 
     #[test]
@@ -157,28 +182,30 @@ mod test {
         assert_eq!(varint4_size(0), Some(1));
         assert_eq!(write_varint4(&mut buf, 0), Some(1));
         assert_eq!(buf, [0, 5, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint4(&mut buf), (0, 1));
+        assert_eq!(read_varint4(&buf), Ok((0, 1)));
 
         assert_eq!(varint4_size(16), Some(1));
         assert_eq!(write_varint4(&mut buf, 16), Some(1));
         assert_eq!(buf, [16, 5, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint4(&mut buf), (16, 1));
+        assert_eq!(read_varint4(&buf), Ok((16, 1)));
 
         assert_eq!(varint4_size(128), Some(2));
         assert_eq!(write_varint4(&mut buf, 128), Some(2));
         assert_eq!(buf, [64, 128, 5, 5, 5, 5, 5, 5]);
-        assert_eq!(read_varint4(&mut buf), (128, 2));
+        assert_eq!(read_varint4(&buf), Ok((128, 2)));
 
         assert_eq!(varint4_size(57_829_138), Some(4));
         assert_eq!(write_varint4(&mut buf, 57_829_138), Some(4));
         assert_eq!(buf, [0x83, 0x72, 0x67, 0x12, 5, 5, 5, 5]);
-        assert_eq!(read_varint4(&mut buf), (57_829_138, 4));
+        assert_eq!(read_varint4(&buf), Ok((57_829_138, 4)));
 
         assert_eq!(varint4_size(2_118_699_314), Some(4));
         assert_eq!(write_varint4(&mut buf, 2_118_699_314), Some(4));
         assert_eq!(buf, [0xfe, 0x48, 0xc9, 0x32, 5, 5, 5, 5]);
-        assert_eq!(read_varint4(&mut buf), (2_118_699_314, 4));
+        assert_eq!(read_varint4(&buf), Ok((2_118_699_314, 4)));
 
         assert_eq!(varint4_size(2_147_483_648), None);
+
+        assert_eq!(read_varint4(&[0xfe]), Err(()));
     }
 }
